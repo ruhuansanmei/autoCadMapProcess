2236d976c70e358636daa00cf3abfcf033737a86
dazongshi <2273056394@qq.com>
2018-10-19 18:02:12 +0800

    开放参数计算方法

--- app/getMapData.ts
+++ app/getMapData.ts
@@ -28,64 +28,64 @@ let getRealCordinate = (x: number, calOption: CalOption) => {
   return a * x + b
 }
 
-// let calculateOption = (samplePair:SamplePair):CalOption =>  {
-//   let {one , two} = samplePair
-//   let x1 = one.x
-//   let y1 = one.y
-//   let x2 = two.x
-//   let y2 = two.y
-//   let a = ( y1 - y2) / (x1 - x2)
-//   let b = (x1*y2 - y1 * x2) / (x1 - x2)
-//   let calOption : CalOption = {a,b}
-//   return calOption
-// }
+let calculateOption = (samplePair:SamplePair):CalOption =>  {
+  let {one , two} = samplePair
+  let x1 = one.x
+  let y1 = one.y
+  let x2 = two.x
+  let y2 = two.y
+  let a = ( y1 - y2) / (x1 - x2)
+  let b = (x1*y2 - y1 * x2) / (x1 - x2)
+  let calOption : CalOption = {a,b}
+  return calOption
+}
 
-// //测试计算结果
+//测试计算结果
 // let calculateOptionTask = () => {
 //   let result = calculateOption(samplePairData)
 //   fs.writeFileSync("./resultData.json",JSON.stringify(result))
 //   console.log("task finish !!!")
 // }
-// // calculateOptionTask()
+// calculateOptionTask()
 
-// let calculateOptionAboutX = () => {
-//   let sourceXStr = fs.readFileSync("./sourceData/sourceX.json",{encoding:"binary"})
-//   let sourceX = JSON.parse(sourceXStr)
-//   let result = calculateOption(sourceX)
-//   fs.writeFileSync("./result/resultX.json",JSON.stringify(result))
-// }
+let calculateOptionAboutX = () => {
+  let sourceXStr = fs.readFileSync("./sourceData/sourceX.json",{encoding:"binary"})
+  let sourceX = JSON.parse(sourceXStr)
+  let result = calculateOption(sourceX)
+  fs.writeFileSync("./result/resultX.json",JSON.stringify(result))
+}
 
-// let calculateOptionAboutY = () => {
-//   let sourceYStr = fs.readFileSync("./sourceData/sourceY.json",{encoding:"binary"})
-//   let sourceY = JSON.parse(sourceYStr)
-//   let result = calculateOption(sourceY)
-//   fs.writeFileSync("./result/resultY.json", JSON.stringify(result))
-// }
+let calculateOptionAboutY = () => {
+  let sourceYStr = fs.readFileSync("./sourceData/sourceY.json",{encoding:"binary"})
+  let sourceY = JSON.parse(sourceYStr)
+  let result = calculateOption(sourceY)
+  fs.writeFileSync("./result/resultY.json", JSON.stringify(result))
+}
 
-// let task = () => {
-//   calculateOptionAboutX()
-//   calculateOptionAboutY()
-//   return "task finish !!!"
-// }
+let task = () => {
+  calculateOptionAboutX()
+  calculateOptionAboutY()
+  return "task finish !!!"
+}
 
-// // task()
-// let calThirdPoint = (lng:number, lat: number) => {
-//   let calOptionXStr  = fs.readFileSync("./result/resultX.json",{encoding:"binary"})
-//   let calOptionX = JSON.parse(calOptionXStr) 
-//   let calOptionYStr = fs.readFileSync("./result/resultY.json",{encoding:"binary"}) 
-//   let calOptionY = JSON.parse(calOptionYStr)
-//   let lntR = getRealCordinate(lng,calOptionX)
-//   let latR = getRealCordinate(lat, calOptionY)
-//   let result = {lntR, latR}
-//   console.log(result)
-//   console.log("task finish !!!")
-// }
+task()
+let calThirdPoint = (lng:number, lat: number) => {
+  let calOptionXStr  = fs.readFileSync("./result/resultX.json",{encoding:"binary"})
+  let calOptionX = JSON.parse(calOptionXStr) 
+  let calOptionYStr = fs.readFileSync("./result/resultY.json",{encoding:"binary"}) 
+  let calOptionY = JSON.parse(calOptionYStr)
+  let lntR = getRealCordinate(lng,calOptionX)
+  let latR = getRealCordinate(lat, calOptionY)
+  let result = {lntR, latR}
+  console.log(result)
+  console.log("task finish !!!")
+}
 
-// let taskR = () => {
-//   calThirdPoint(-17630,6730)
-// }
+let taskR = () => {
+  calThirdPoint(116.86251424249804,2.755818522219295)
+}
 
-// taskR()
+taskR()
 
 
 
