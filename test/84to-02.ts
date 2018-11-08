c40ba9757f6d1db571cd16c724775f49e33ae274
dazongshi <2273056394@qq.com>
2018-11-08 15:55:35 +0800

    处理海淀数据

--- test/84to-02.ts
+++ test/84to-02.ts
@@ -5,14 +5,18 @@ let t84_02 = () => {
     let dataSource = fs.readFileSync('./haidian.geojson','utf-8')
     let data = JSON.parse(dataSource)  
     data.features.map(feature => {
-      feature.geometry.coordinates.map(polygen => polygen.map(point => {
-        let  newPoint =  wgs84togcj02(...point)
-        point[0] = newPoint[0]
-        point[1] = newPoint[1]
-        console.log(point)
-        // throw("break")
-
-      }))
+      feature.geometry.coordinates.map(multi => {
+        multi.map(polygen => polygen.map(point => {
+          let  newPoint =  wgs84togcj02(...point)
+          if (!newPoint[0]) {
+            console.log(point)
+          }
+          point[0] = newPoint[0] + 0.0014163143596789814
+          point[1] = 0.9999995194772249 * newPoint[1] + 0.0000520958819648871
+          // console.log(point)
+          // throw("break")
+        }))
+      })
     })
     fs.writeFileSync('./rrr.json',JSON.stringify(data))
     console.log('finish')  
